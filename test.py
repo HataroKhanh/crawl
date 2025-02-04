0059ee943aa7ed64f2884a0d6d03c8b90e7dc0f8
khanh <khanhdacodau2007@gmail.com>
2025-02-04 19:13:25 +0700

    #

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
-from xuongmoc import Crawler
-
-
-crawer  =  Crawler()
-    
-print(crawer.crawl_post("https://xuongmocdct.com.vn/goc-chia-se-cach-lam-ban-tra-may-don-gian-tai-nha.html","123.com"))
+import xuongmoc
+import json
+Crawler = xuongmoc.Crawler(
+)
+payload = {
+        "page": 1,
+        "slug": "tin-tuc",
+        "taxonomy": "category",
+        "get": "more"
+    }
+data = Crawler.crawl_post_links("https://xuongmocdct.com.vn/load-more-cat-post",payload)
+with open('links.json',"w",encoding="utf-8") as f:
+    json.dump(f)
